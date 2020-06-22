20afc80d2da80f51c47935ba324394c947e564fe
LazarPopadic97 <lazarpopadic09@gmail.com>
2020-06-22 16:36:17 +0200

    Uradjena dokumentacija za klasu MyProfileAction

--- src/main/java/popadic/lazar/fon/fisweb/action/impl/MyProfileAction.java
+++ src/main/java/popadic/lazar/fon/fisweb/action/impl/MyProfileAction.java
@@ -11,12 +11,18 @@ import popadic.lazar.fon.fisweb.action.AbstractAction;
 import popadic.lazar.fon.fisweb.constants.PageConstants;
 
 /**
- *
+ * Akcija za prikaz profila korisnika, nasledjuje apstraktnu klasu AbstractAction
+ * 
  * @author Lazar Popadic
  */
 @Component
 public class MyProfileAction extends AbstractAction{
 
+    /**
+     * Vraca izgled stranice za prikaz informacija o korisniku
+     * @param request http zahtev 
+     * @return stranicu za prikaz korisnika kao string
+     */
     @Override
     public String execute(HttpServletRequest request) {
         return PageConstants.VIEW_PROFILE;
