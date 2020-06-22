effe132c57abafa2d23e99bc88006dc2f487a341
LazarPopadic97 <lazarpopadic09@gmail.com>
2020-06-22 16:35:12 +0200

    Uradjena dokumentacija za klasu EditDepartmentAction

--- src/main/java/popadic/lazar/fon/fisweb/action/impl/EditDepartmentAction.java
+++ src/main/java/popadic/lazar/fon/fisweb/action/impl/EditDepartmentAction.java
@@ -13,18 +13,27 @@ import popadic.lazar.fon.fisweb.constants.PageConstants;
 import popadic.lazar.fon.fisweb.model.Department;
 
 /**
- *
+ * Akcija za izmenu stavki katedre, nasledjuje apstraktnu klasu AbstractAction
+ * 
  * @author Lazar Popadic
  */
 @Component
 public class EditDepartmentAction extends AbstractAction {
 
+    /**
+     * Neparametrizovani konstruktor
+     */
     public EditDepartmentAction() {
     }
 
+    /**
+     * Vraca stranicu za izmenu katedre
+     * @param request http zahtev
+     * @return stranicu za izmenu stavki katedre kao string
+     */
     @Override
     public String execute(HttpServletRequest request) {
-          Department d = getDepartment(request);
+        Department d = getDepartment(request);
 
         request.setAttribute("id", d.getId());
         request.setAttribute("name", d.getName());
@@ -32,6 +41,12 @@ public class EditDepartmentAction extends AbstractAction {
         return PageConstants.VIEW_EDIT_DEPARTMENT;
     }
 
+    /**
+     * Vraca odgovarajucu katedru
+     * @param request
+     * @return odgovarajucu katedru
+     * @throws NumberFormatException ako je unos id-a nepravilan
+     */
     private Department getDepartment(HttpServletRequest request) throws NumberFormatException {
         long id = Long.valueOf(request.getPathInfo().split("/")[3]);
         Department department = new Department();
