18a838c3ddddb078c4c154d38817f94d048a9834
LazarPopadic97 <lazarpopadic09@gmail.com>
2020-06-23 13:04:00 +0200

    Napravljen json fajl za skladistenje korisnika i omoguceno citanje iz istog

--- src/main/java/popadic/lazar/fon/fisweb/listener/MyApplicationContextListener.java
+++ src/main/java/popadic/lazar/fon/fisweb/listener/MyApplicationContextListener.java
@@ -5,7 +5,13 @@
  */
 package popadic.lazar.fon.fisweb.listener;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
@@ -13,6 +19,7 @@ import javax.servlet.annotation.WebListener;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import popadic.lazar.fon.fisweb.config.MyAppConfig;
+import popadic.lazar.fon.fisweb.constants.PageConstants;
 import popadic.lazar.fon.fisweb.model.Department;
 import popadic.lazar.fon.fisweb.model.User;
 
@@ -30,7 +37,11 @@ public class MyApplicationContextListener implements ServletContextListener {
         System.out.println("=======================================================");
     }
 
+    //domneske detaljno testiranje i java doc 
+    //liste da se povlace iz json fajla 
+    //testovi za akcije(osnovne scenarije) i java doc
     @Override
+    
     public void contextInitialized(ServletContextEvent sce) {
         System.out.println("======================================================");
         System.out.println("=========        contextInitialized  =================");
@@ -38,7 +49,6 @@ public class MyApplicationContextListener implements ServletContextListener {
         //lista korisnika
         sce.getServletContext().setAttribute("users", createUsers());
         sce.getServletContext().setAttribute("departments", createDepartments());
-        
         //ucitaj spring kontekst
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(MyAppConfig.class);
         sce.getServletContext().setAttribute("applicationContext", applicationContext);
@@ -50,13 +60,22 @@ public class MyApplicationContextListener implements ServletContextListener {
     }
     
     private List<User> createUsers(){
-        return new ArrayList<User>(){
-            {
-                add(new User("pera","peric", "pera", "pera"));
-                add(new User("sofija","sofic", "sofija", "sofija"));
-                add(new User("jovan","jovanic", "jovan", "jovan"));
-            }
-        };
+        List<User>listaKorisnika=new ArrayList<>();
+        try(FileReader in = new FileReader(PageConstants.PAGE_JSON)){
+            Gson gson=new Gson();
+            User[] korisnici=gson.fromJson(in,User[].class);
+            listaKorisnika=Arrays.asList(korisnici);
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        return listaKorisnika;
+//        return new ArrayList<User>(){
+//            {
+//                add(new User("Petar ","Peric", "peraperic@gmail.com", "pera"));
+//                add(new User("Sofija","Sofic", "sofijasofic251@gmail.com", "sofija"));
+//                add(new User("Lazar","Popadic", "laza", "laza"));
+//            }
+//        };
     }
     private List<Department> createDepartments(){
         return new ArrayList<Department>(){
